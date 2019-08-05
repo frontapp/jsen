370322b6303e3f7f747dd2a8bae054ff0647fb6a
Christophe Venevongsos <christophe@frontapp.com>
2019-08-05 13:59:31 +0200

    update dist

--- dist/jsen.js
+++ dist/jsen.js
@@ -1,13 +1,13 @@
 /*
  * jsen
  * https://github.com/bugventure/jsen
- * v0.6.6
+ * v0.6.6-1
  *
  * Copyright (c) 2016 Veli Pehlivanov <bugventure@gmail.com>
  * Licensed under the MIT license
  */
 
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.jsen = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.jsen = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 module.exports = require('./lib/jsen.js');
 },{"./lib/jsen.js":5}],2:[function(require,module,exports){
 'use strict';
@@ -1199,7 +1199,9 @@ ValidationContext.prototype.generate = function () {
 
                 encodedFormat = encodeStr(schema.format);
 
-                this.code('if (!formats[' + encodedFormat + '](' + path + ', schemas[' + encodedFormat + '])) {');
+                this.code('if (!formats[' + encodedFormat + '](' + path + ', schemas[' + encodedFormat + '], function (val) {');
+                this.code(context.path + ' = val')
+                this.code('})) {');
                 this.error('format');
                 this.code('}');
             }
